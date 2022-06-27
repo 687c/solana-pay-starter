7b26ee1a08d4e9b43009358472d40a7464ac8480
jim4067 <jimmyimpulse2@gmail.com>
2022-06-27 11:54:11 +0300

    continue

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,40 @@
 import React, { useMemo } from "react";
+import { WalletAdapter, WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 
 const App = ({ Component, pageProps }) => {
 
-  return (
-    <Component {...pageProps} />
-  );
+	//set the network config
+	const network = WalletAdapterNetwork.Devnet;
+
+	//set the endpoint
+	const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+	//set the wallet that will be compiled and that the user will see 
+	const wallets = useMemo(() => [
+		new PhantomWalletAdapter,
+		new GlowWalletAdapter,
+		new SlopeWalletAdapter,
+		new SolflareWalletAdapter({ network }),
+		new TorusWalletAdapter
+	], [network]);
+
+	return (
+		<ConnectionProvider endpoint={endpoint}>
+			<WalletProvider wallets={wallets} autoConnect>
+				<WalletModalProvider>
+					<Component {...pageProps} /> 
+				</WalletModalProvider>
+			</WalletProvider>
+		</ConnectionProvider>
+	);
 };
 
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,58 @@
 import React from "react";
+import { PublicKey } from "@solana/web3.js";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 import HeadComponent from '../components/Head';
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "jimii_47";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
-  return (
-    <div className="App">
-      <HeadComponent/>
-      <div className="container">
-        <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
-        </header>
 
-        <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
-        </main>
+	const { PublicKey } = useWallet();
 
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+	console.log("this is the public Key", PublicKey);
+
+	const renderNotConnectedContainer = () => (
+		<>
+			<img src="https://media.giphy.com/media/143vPc6b08locw/giphy.gif" alt="emoji" />
+			<div className='button-container'>
+				<WalletMultiButton className="cta-button connect-wallet-button" />
+			</div>
+		</>
+	)
+
+
+	return (
+		<div className="App">
+			<HeadComponent />
+			<div className="container">
+				<header className="header-container">
+					<p className="header"> 😳 Evil Empire!!!😈</p>
+					<br />
+					<p className="header"> 😂 Laughing all the way to the bank 😂 </p>
+					<p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+				</header>
+
+				<main>
+					{/* <img src="https://media.giphy.com/media/143vPc6b08locw/giphy.gif" alt="emoji" /> */}
+					{PublicKey ? "Connected" : renderNotConnectedContainer()}
+				</main>
+
+				<div className="footer-container">
+					<img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+					<a
+						className="footer-text"
+						href={TWITTER_LINK}
+						target="_blank"
+						rel="noreferrer"
+					>{`built on @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
